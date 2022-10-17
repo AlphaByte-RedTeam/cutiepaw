6306a1cfd6c05d9f55575c77992342c74b6446fd
janeclrst <janiceclaresta10@gmail.com>
2022-10-17 16:18:42 +0700

    feat: add catalogue activity

---
+++ app/src/main/java/com/kelompoktiga/cutiepaw/CatalogueActivity.kt
@@ -0,0 +1,11 @@
+package com.kelompoktiga.cutiepaw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CatalogueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalogue)
+    }
+}
