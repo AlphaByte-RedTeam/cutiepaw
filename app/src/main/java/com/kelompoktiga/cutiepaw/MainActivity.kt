10a8bd1575d1ec0116cdf05f28190ecd2209fe85
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-10-18 18:18:08 +0700

    feat: send intent to register activity

--- app/src/main/java/com/kelompoktiga/cutiepaw/MainActivity.kt
+++ app/src/main/java/com/kelompoktiga/cutiepaw/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.kelompoktiga.cutiepaw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val getStarted: Button = findViewById(R.id.btnGetStarted)
+
+        getStarted.setOnClickListener {
+            val sendIntent = Intent(this, RegisActivity::class.java)
+            startActivity(sendIntent)
+        }
     }
 }
