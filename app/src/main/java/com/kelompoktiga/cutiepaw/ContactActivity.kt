6acdc0489698fcc06ffb354d96fe936c6d8d8316
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-10-18 13:29:56 +0700

    feat: make email button functional

--- app/src/main/java/com/kelompoktiga/cutiepaw/ContactActivity.kt
+++ app/src/main/java/com/kelompoktiga/cutiepaw/ContactActivity.kt
@@ -1,11 +1,36 @@
 package com.kelompoktiga.cutiepaw
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+
+        val email: Button = findViewById(R.id.btnMail)
+
+        email.setOnClickListener() {
+            sendEmail()
+        }
+    }
+
+    private fun sendEmail() {
+        val emailRecipient: String = "s32200091@student.ubm.ac.id"
+        val emailSubject: String = Uri.encode("Cutie Paw")
+        val emailMsg: String = Uri.encode("Hello, I have a question about your app.")
+        val sendTo = "mailto:$emailRecipient?&subject=$emailSubject&body=$emailMsg"
+        val sendEmailIntent = Intent(Intent.ACTION_SENDTO)
+        sendEmailIntent.setData(Uri.parse(sendTo))
+        try {
+            startActivity(Intent.createChooser(sendEmailIntent, "Sending email..."))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(this, "Error sending email...", Toast.LENGTH_SHORT).show()
+        }
     }
 }
