35a6533616fe23ed2c7f009b26f702d47e55a780
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-10-22 22:10:45 +0700

    refactor: refactor product list into separate file

--- app/src/main/java/com/kelompoktiga/cutiepaw/CatalogueActivity.kt
+++ app/src/main/java/com/kelompoktiga/cutiepaw/CatalogueActivity.kt
@@ -13,18 +13,6 @@ import android.widget.ImageView
 import android.widget.TextView
 
 class CatalogueActivity : AppCompatActivity() {
-
-    private val productsList = arrayOf(
-        Product(R.drawable.pet_food, "Pawly - Dog Food DQP", "Rp 99.999"),
-        Product(R.drawable.pet_food, "Pawly - Dog Food DQP", "Rp 99.999"),
-        Product(R.drawable.pet_food, "Pawly - Dog Food DQP", "Rp 99.999"),
-        Product(R.drawable.pet_food, "Pawly - Dog Food DQP", "Rp 99.999"),
-        Product(R.drawable.pet_food, "Pawly - Dog Food DQP", "Rp 99.999"),
-        Product(R.drawable.pet_food, "Pawly - Dog Food DQP", "Rp 99.999"),
-        Product(R.drawable.pet_food, "Pawly - Dog Food DQP", "Rp 99.999"),
-        Product(R.drawable.pet_food, "Pawly - Dog Food DQP", "Rp 99.999"),
-    )
-
     private var myProductList = ArrayList<Product>()
 
     private var customAdapter: CustomAdapter? = null
@@ -56,7 +44,7 @@ class CatalogueActivity : AppCompatActivity() {
     class CustomAdapter(
         private var imgList: ArrayList<Product>,
         var context: Context
-        ): BaseAdapter() {
+    ) : BaseAdapter() {
 
         override fun getCount(): Int {
             return imgList.size
@@ -73,7 +61,8 @@ class CatalogueActivity : AppCompatActivity() {
         override fun getView(position: Int, p1: View?, viewGroup: ViewGroup?): View {
             var view = p1
             if (view == null)
-                view = LayoutInflater.from(context).inflate(R.layout.row_grid_item, viewGroup, false)
+                view =
+                    LayoutInflater.from(context).inflate(R.layout.row_grid_item, viewGroup, false)
             val imgView = view!!.findViewById<ImageView>(R.id.imgProduct)
             val tvProduct = view.findViewById<TextView>(R.id.txtProductName)
             val tvPrice = view.findViewById<TextView>(R.id.txtProductPrice)
---
+++ app/src/main/java/com/kelompoktiga/cutiepaw/ProductsList.kt
@@ -0,0 +1,104 @@
+package com.kelompoktiga.cutiepaw
+
+val productsList = arrayOf(
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+    Product(
+        R.drawable.pet_food,
+        "Pedigree",
+        "$27.78",
+        "Complete and balanced nutrition has antioxidants, vitamins and minerals to help maintain a healthy lifestyle.\n" +
+                "Optimal levels of omega-6 fatty acid nourish the skin and help keep his coat shiny and healthy.\n" +
+                "Whole grains and a special fiber blend support healthy digestion with a delicious roasted chicken flavor.\n" +
+                "Unique, crunchy texture helps clean the teeth with every bite to support good oral health between brushings.\n" +
+                "Proudly made in the USA with the world’s finest ingredients; contains no high fructose corn syrup, artificial flavors or sugar."
+    ),
+)
